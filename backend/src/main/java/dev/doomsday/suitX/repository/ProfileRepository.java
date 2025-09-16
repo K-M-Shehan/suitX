690e394d143f7a4c80ae8575cff13aae2d1edefc
K-M-Shehan <miweshshehan@gmail.com>
2025-09-16 09:10:06 +0530

    Revert "setting,profile"
    This reverts commit d048772efdb3b56751105d4401ba61183737dc2d.

--- backend/src/main/java/dev/doomsday/suitX/repository/ProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.doomsday.suitX.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProfileRepository extends MongoRepository<ProfileEntity, String> {
-    // Repository skeleton
-}
