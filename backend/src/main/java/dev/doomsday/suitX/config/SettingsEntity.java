d0c05110ad064bafd4a95438e31859b0b985036f
thisanth09 <m.thisanth09@gmail.com>
2025-09-16 13:04:02 +0530

    setting and profile

---
+++ backend/src/main/java/dev/doomsday/suitX/config/SettingsEntity.java
@@ -0,0 +1,22 @@
+package dev.doomsday.suitX.config;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "settings")
+public class SettingsEntity {
+
+    @Id
+    private String id;
+
+    private Account account;
+    private Notification notification;
+    private ContactUs contactUs;
+
+    // Sub-classes as inner classes
+    public static class Account { }
+    public static class Notification { }
+    public static class ContactUs { }
+
+    // Getters and setters can be added later
+}
